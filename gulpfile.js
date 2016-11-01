5661aabe393bdbb6b23a7e642018731523e65dd3
leodjam <leonard.djamdjian@hetic.net>
2016-11-01 22:34:10 +0100

    add gulp task, fix indent

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,47 @@
 var gulp = require('gulp'),
-sass = require('gulp-sass');
-sync = require('browser-sync').create();
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
+    sass = require('gulp-sass');
+    sync = require('browser-sync').create();
+    postcss = require('gulp-postcss');
+    autoprefixer = require('autoprefixer');
+    mincss = require('gulp-minify-css');
+    concat = require('gulp-concat');
+    rename = require('gulp-rename');
+    uglify = require('gulp-uglify');
 
 var processors = [
 autoprefixer
 ];
 
-gulp.task('jquery', function () {
-    return gulp.src('./node_modules/jquery/src')
-        .pipe(jquery({
-            flags: ['-deprecated', '-event/alias', '-ajax/script', '-ajax/jsonp', '-exports/global']
-        }))
-        .pipe(gulp.dest('./public/vendor/'));
-    // creates ./public/vendor/jquery.custom.js 
+gulp.task('scripts', function() {
+    return gulp.src('app/js/*.js')
+    .pipe(concat('scripts.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+    .pipe(sync.stream());
 });
+
+gulp.task('vendors', function () {
+    return gulp.src('app/lib/jquery.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/public/vendor/'));
+});
+
 gulp.task('scss', function() {
-return gulp.src('app/scss/style.scss')
-.pipe(sass())
-.pipe(postcss(processors))
-.pipe(gulp.dest('dist/css'))
-.pipe(sync.stream());
+    return gulp.src('app/scss/style.scss')
+    .pipe(sass())
+    .pipe(postcss(processors))
+    .pipe(mincss())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(sync.stream());
 });
 
-gulp.task('sync', ['scss'], function(){
-		sync.init({
-			server:'./'
-		})
-		gulp.watch("app/scss/*.scss", ['scss']);
+gulp.task('sync', ['vendors', 'scripts', 'scss'], function(){
+	sync.init({
+		server:'./'
+	})
+	gulp.watch("app/scss/*.scss", ['scss']);
+    gulp.watch("app/js/*.js", ['scripts']);
+});
 
+gulp.task('default', ['sync'], function() {
 });
